662fe6138f6e91253605f899f2c1dc973b7a4ce0
Márk Paróczi <mark.paroczi@gmail.com>
2019-12-05 14:10:38 +0100

    Implement FileWordAnalyzer constructor

--- src/com/codecool/file_part_reader/FileWordAnalyzer.java
+++ src/com/codecool/file_part_reader/FileWordAnalyzer.java
@@ -2,4 +2,8 @@ package com.codecool.file_part_reader;
 
 public class FileWordAnalyzer {
     private FilePartReader reader;
+
+    public FileWordAnalyzer(FilePartReader reader) {
+        this.reader = reader;
+    }
 }
