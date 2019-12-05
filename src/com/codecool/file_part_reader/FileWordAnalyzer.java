cd8a4981355866539b64b5e91c58e9615a38b0ff
Márk Paróczi <mark.paroczi@gmail.com>
2019-12-05 14:47:15 +0100

    Implement getWordsOrderedAlphabetically() method

--- src/com/codecool/file_part_reader/FileWordAnalyzer.java
+++ src/com/codecool/file_part_reader/FileWordAnalyzer.java
@@ -1,9 +1,24 @@
 package com.codecool.file_part_reader;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWordAnalyzer {
     private FilePartReader reader;
 
     public FileWordAnalyzer(FilePartReader reader) {
         this.reader = reader;
+    }
+
+    public List getWordsOrderedAlphabetically() {
+        String content = reader.readLines();
+
+        List<String> words = new ArrayList<>(Arrays.asList(content.replaceAll( "\\p{Punct}", "").toLowerCase().split("\\s+")));
+
+        Collections.sort(words);
+
+        return words;
     }
 }
