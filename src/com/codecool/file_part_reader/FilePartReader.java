08bc97362c6594c4951027565351d9992a4a93c3
Márk Paróczi <mark.paroczi@gmail.com>
2019-12-05 09:43:27 +0100

    Implement setup(String filePath, int fromLine, int toLine) method

--- src/com/codecool/file_part_reader/FilePartReader.java
+++ src/com/codecool/file_part_reader/FilePartReader.java
@@ -4,4 +4,10 @@ public class FilePartReader {
     private String filePath;
     private int fromLine;
     private int toLine;
+
+    public void setup(String filePath, int fromLine, int toLine) {
+        this.filePath = filePath;
+        this.fromLine = fromLine;
+        this.toLine = toLine;
+    }
 }
