81baaf2a0d53aa9e201ed8af67cf7f86d2f31c0b
Márk Paróczi <mark.paroczi@gmail.com>
2019-12-07 14:17:19 +0100

    Add testReaderIsNotNull() test method

--- tests/com/codecool/file_part_reader/FilePartReaderTest.java
+++ tests/com/codecool/file_part_reader/FilePartReaderTest.java
@@ -1,7 +1,19 @@
 package com.codecool.file_part_reader;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilePartReaderTest {
+    private String pathToFile = "C:\\Users\\Márk\\Desktop\\Iskola\\Codecool\\3. - OOP\\5TH SI Week\\FilePartReader testing with JUnit\\src\\com\\codecool\\file_part_reader\\input.txt";
 
+    @Test
+    @DisplayName("Test the FilePartReader class' constructor")
+    @Tag("constructor")
+    void testReaderIsNotNull() {
+        FilePartReader reader = new FilePartReader(pathToFile, 1, 5);
+        assertNotNull(reader);
+    }
 }
